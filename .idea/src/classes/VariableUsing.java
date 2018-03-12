ad56bf8dda973dbba52a9cf64a90c540763ff31e
Matesteusz <anton.mateusz@o2.pl>
2018-03-12 22:26:46 +0100

    Variable using script

---
+++ .idea/src/classes/VariableUsing.java
@@ -0,0 +1,20 @@
+package classes;
+
+/**
+ * Created by Matesteusz on 12.03.2018.
+ */
+public class VariableUsing {
+    public static void main(String args[]) {
+
+        int var1; //deklaracja zmiennej
+        int var2;
+
+        var1 = 1024; //przypisanie wartosci do zmiennej
+
+        System.out.println("var1 zawiera" + var1);
+
+        var2 = var1 / 2;
+
+        System.out.println("var2 zawiera var 1 / 2: " + var2);
+    }
+}
