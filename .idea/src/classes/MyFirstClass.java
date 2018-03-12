6f5503c41b8c43ae9425787e8871ffc9af7132e6
Matesteusz <anton.mateusz@o2.pl>
2018-03-12 20:44:23 +0100

    My first Java Script

---
+++ .idea/src/classes/MyFirstClass.java
@@ -0,0 +1,11 @@
+package Classes;
+
+/**
+ * Created by Matesteusz on 10.03.2018.
+ */
+public class MyFirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello Universe!");
+    }
+
+}
