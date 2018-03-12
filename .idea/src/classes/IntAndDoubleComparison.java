80db3d4f5cf76d661eb1415080a600453c810ea0
Matesteusz <anton.mateusz@o2.pl>
2018-03-12 22:43:17 +0100

    int and double comparison

---
+++ .idea/src/classes/IntAndDoubleComparison.java
@@ -0,0 +1,22 @@
+package classes;
+
+/**
+ * Created by Matesteusz on 12.03.2018.
+ */
+public class IntAndDoubleComparison {
+    public static void main(String args []){
+        int x = 10;
+        double y = 10.0;
+
+        System.out.println("x to: " + x);
+        System.out.println( "y to: " + y);
+        System.out.println(); //pusta linijka
+
+
+        x = x / 4;
+        y = y / 4;
+
+        System.out.println("x po dzieleniu: "+ x);
+        System.out.println("y po dzieleniu: " + y);
+    }
+}
--- .idea/src/classes/VariableUsing.java
+++ .idea/src/classes/VariableUsing.java
@@ -6,15 +6,16 @@ package classes;
 public class VariableUsing {
     public static void main(String args[]) {
 
-        int var1; //deklaracja zmiennej
+        int var1 = 1024; //deklaracja zmiennej
         int var2;
 
-        var1 = 1024; //przypisanie wartosci do zmiennej
+//        var1 = 1024; //przypisanie wartosci do zmiennej
 
         System.out.println("var1 zawiera" + var1);
 
         var2 = var1 / 2;
 
-        System.out.println("var2 zawiera var 1 / 2: " + var2);
+        System.out.print("var2 zawiera var 1 / 2: ");
+        System.out.println(var2);
     }
 }
