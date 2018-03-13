7eb173a4cb07867ea5087850527a4a5f21f0b4d7
Matesteusz <anton.mateusz@o2.pl>
2018-03-13 18:38:18 +0100

    If Else Instruction

---
+++ .idea/src/classes/ForLoop.java
@@ -0,0 +1,14 @@
+package classes;
+
+/**
+ * Created by Matesteusz on 13.03.2018.
+ */
+public class ForLoop {
+    public static void main(String args []){
+        int a;
+
+        for(a = 0; a < 5; a ++)
+            System.out.println("a is equal to: " + a);
+        System.out.println("Loop completed");
+    }
+}
