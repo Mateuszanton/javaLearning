7fa869eae8d0f57b2541db55d48e57ffa44dc4d7
Matesteusz <anton.mateusz@o2.pl>
2018-03-13 18:06:08 +0100

    Gallon to litres class addition

---
+++ .idea/src/classes/GalToLitres.java
@@ -0,0 +1,17 @@
+package classes;
+
+/**
+ * Created by Matesteusz on 13.03.2018.
+ */
+public class GalToLitres {
+    public static void main(String args[]){
+        double gallons;
+        double litres;
+
+        gallons = 10;
+
+        litres = gallons * 3.7854;
+
+        System.out.println(gallons + " galonów odpowiada " + litres + "litrom");
+    }
+}
