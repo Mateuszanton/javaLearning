2cac9e45833af469e336737d3d0a3f698e8fd03e
Matesteusz <anton.mateusz@o2.pl>
2018-03-14 21:33:00 +0100

    Code Block comment added

--- .idea/src/classes/CodeBlock.java
+++ .idea/src/classes/CodeBlock.java
@@ -10,13 +10,12 @@ public class CodeBlock {
         i = 5;
         j = 10;
 
-        //instruction condition which decides of code execution
-        for (i = 5; i > -1; i--) {
+        //instruction condition which decides of code block execution
+        for (i = 5; i > -1; i --) {
             if (i > 0) {
-                System.out.println("i is not equal 0");
-                k = j / i;
-
-                System.out.println("j / i is: " + k);
+                System.out.println("i is not equal 0"); // this
+                k = j / i;                              // is
+                System.out.println("j / i is: " + k);   // the code block
             } else
                 System.out.println("i equal to 0");
 
