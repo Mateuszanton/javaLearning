7e6226c2bb1faa1ce42fa6c2c5ca0cfddd9f9dba
Matesteusz <anton.mateusz@o2.pl>
2018-03-14 21:54:04 +0100

    Counter Usage class added

---
+++ .idea/src/classes/CounterUsage.java
@@ -0,0 +1,28 @@
+package classes;
+
+/**
+ * Created by Matesteusz on 13.03.2018.
+ */
+public class CounterUsage {
+    public static void main(String args[]) {
+        double gallons;
+        double litres;
+        int counter;
+
+        counter = 0;
+
+        for (gallons = 1; gallons <= 100; gallons++) {
+            litres = gallons * 3.7854;
+
+            System.out.println(gallons + "gallons is: " + litres);
+
+            counter++;
+
+// every 10th counter displays empty line
+            if (counter == 10) {
+                System.out.println();
+                counter = 0; //counter reset
+            }
+        }
+    }
+}
