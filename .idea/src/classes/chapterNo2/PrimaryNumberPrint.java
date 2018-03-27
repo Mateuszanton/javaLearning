5a94578f2478d6a3b9c7d1e13e46da8d4459e5c4
Matesteusz <anton.mateusz@o2.pl>
2018-03-27 20:18:08 +0200

    Primary number

--- .idea/src/classes/chapterNo2/PrimaryNumberPrint.java
+++ .idea/src/classes/chapterNo2/PrimaryNumberPrint.java
@@ -1,15 +1,18 @@
 package classes.chapterNo2;
 
 public class PrimaryNumberPrint {
-    public static void main(String args[]) {
-        int x;
-        int y = 0;
-        double div = 0;
-        System.out.println("primary numbers:  ");
-        for (x = 2; x <= 100; x++) {
-            for (y = 1; y < x; y++)
-                if (x % x == 0 && x % 1 == 0 && x % y == 0)
-            System.out.println(x);
+         public static void main(String args[]) {
+            int i,j;
+            for (i = 2; i < 100; i++) {
+                boolean a = true;
+                for (j = 2; j < i; j++) {
+                    if (i % j == 0) {
+                        a = false;
+                    }
+                }
+                if (a) {
+                    System.out.println(i + ", ");
+                }
+            }
         }
     }
-}
