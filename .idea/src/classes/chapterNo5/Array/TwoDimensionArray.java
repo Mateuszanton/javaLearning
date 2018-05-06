72e7b6de00ee1066f23705710302c0b1cd429d1b
Matesteusz <anton.mateusz@o2.pl>
2018-05-06 21:38:38 +0200

    ChapterNo5: Irregular array script added

--- .idea/src/classes/chapterNo5/Array/TwoDimensionArray.java
+++ .idea/src/classes/chapterNo5/Array/TwoDimensionArray.java
@@ -2,17 +2,14 @@ package classes.chapterNo5.Array;
 
 public class TwoDimensionArray {
     // the two dimension array is a list of one dimension array
-    public static void main(String args[]){
+    public static void main(String args[]) {
         // declaration of the table:
-
-        int table[][] = new int[3][4];
-        int t, i;
-
-        for (t = 0; t < 3; t++) {
-            for (i = 0; i < 4; ++i) {
-                table[t][i] = (t * 4) + i + 1;
-                System.out.print(table[t][i] + " ");
-
+        int table[][] = new int[5][6];
+        int a, b, c, d;
+        for (a = 0; a < 5; a++) {
+            for (b = 0; b < 6; ++b) {
+                table[a][b] = (a * 6) + b + 1;
+                System.out.print(table[a][b] + " ");
             }
             System.out.println();
         }
